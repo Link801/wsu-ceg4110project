f2a0be1719aeb2075956b38842b491d729514ccd
Jon Poston <poston.14@wright.edu>
2017-11-06 14:09:41 -0500

    created user interface file and created the user interface

--- UserInterface/src/userinterface/UserInterface.java
+++ UserInterface/src/userinterface/UserInterface.java
@@ -4,18 +4,42 @@
  * and open the template in the editor.
  */
 package userinterface;
-
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import java.awt.BorderLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Frame;
 /**
  *
  * @author Jon
  */
-public class UserInterface {
+public class UserInterface extends Frame{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        new UserInterface();
     }
-    
+    public UserInterface(){
+        JFrame userInterface = new JFrame();
+        userInterface.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        userInterface.setTitle("SeeFood AI UserInterface");
+        userInterface.setSize(800, 750);
+        userInterface.setLocationRelativeTo(null);
+        JButton userButton = new JButton("Add Image to Client");
+        userButton.addActionListener(new ActionListener(){
+           @Override 
+            public void actionPerformed(ActionEvent event) {
+                
+            }
+        });
+        userInterface.add(userButton,BorderLayout.SOUTH);
+        userInterface.setVisible(true);
+    }
 }
